a9f659698a88575a963a73770eaf996c0a8371ad
slowhand1993 <nermankovacevic@live.com>
2022-06-22 14:10:01 +0200

    added new task

---
+++ dwadad.js
@@ -0,0 +1,52 @@
+class Car {
+    numberOfDoors(numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    };
+    fuelQuantity(fuelQuant) {
+        this.fuelQuant = fuelQuant;
+    }
+
+    getNumOfDoors() {
+        return this.numberOfDoors;
+    };
+    getFuelQuantity() {
+        return this.fuelQuant;
+    }
+    
+}
+
+class Volvo extends Car {
+    speedLimit;
+    constructor(speedLimit){
+        super()
+        this.speedLimit = speedLimit;
+    }
+}
+
+class Mercedes extends Car {
+    color = 'red';
+    constructor(color){
+        super();
+        this.color = color;
+        
+    }
+    
+}
+
+let volvo = new Volvo(154);
+volvo.numberOfDoors(4);
+volvo.fuelQuantity('15l');;
+console.log(volvo)
+
+var mercedes = new Mercedes('white');
+//mercedes.color = 3;
+
+//mercedes.numberOfDoors(6);
+//console.log(mercedes)
+
+var automobil = new Mercedes();
+automobil = mercedes;
+//console.log(automobil.getNumOfDoors());
+automobil = volvo;
+//console.log(automobil.speedLimit);
+//console.log(automobil);
